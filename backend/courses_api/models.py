975f8eb8f428f3fb10944da7123f91053498de2f
Thi Huỳnh Thái <19522256@gm.uit.edu.vn>
2021-10-11 22:32:44 +0700

    add Student model

--- backend/courses_api/models.py
+++ backend/courses_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Models):
+    MSSV=models.CharField(max_length=50,unique=True)
+    Name=models.CharField(max_length=200)
+    email=models.EmailField(max_length=254)
+    phone = models.PhoneNumberField()
+    
+
+    def __str__(self):
+        return "{0} {1}".format(self.Name,self.MSSV)
