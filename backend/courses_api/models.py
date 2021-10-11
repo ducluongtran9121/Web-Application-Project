697313526793457bc2c5eaa61ca24fddfc594b09
Thi Huỳnh Thái <19522256@gm.uit.edu.vn>
2021-10-11 22:43:52 +0700

    modify Student model

--- backend/courses_api/models.py
+++ backend/courses_api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Student(models.Models):
-    MSSV=models.CharField(max_length=50,unique=True)
-    Name=models.CharField(max_length=200)
+class Student(models.Model):
+    mssv=models.CharField(max_length=50,unique=True)
+    name=models.CharField(max_length=200)
     email=models.EmailField(max_length=254)
-    phone = models.PhoneNumberField()
-    
+    image=models.ImageField()
+
 
     def __str__(self):
         return "{0} {1}".format(self.Name,self.MSSV)
