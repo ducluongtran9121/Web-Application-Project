6ecbabfe641ec4c9cc37a6f67579d37d75c84325
Thi Huỳnh Thái <19522256@gm.uit.edu.vn>
2021-10-11 23:41:18 +0700

    add url and image path config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'img')
+MEDIA_URL = '/img/'
