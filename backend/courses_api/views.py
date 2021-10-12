d0fa7648d17d7765a2dde971389777d26b9db420
Thi Huỳnh Thái <19522256@gm.uit.edu.vn>
2021-10-12 16:10:49 +0700

    add Student serializer test

--- backend/courses_api/views.py
+++ backend/courses_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from .serializers import StudentSerializer
+from .models import *
 # Create your views here.
+
+class StudentAPIView(APIView):
+
+    def get(self,request):
+        studentList = Student.objects.all()
+        serializer = StudentSerializer(studentList,many=True)
+        print(serializer)
+        return Response(serializer.data)
