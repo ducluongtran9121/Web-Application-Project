2cf88570e91a59f12dc3d5269b505cb1a06191bd
Thi Huỳnh Thái <19522256@gm.uit.edu.vn>
2021-10-14 16:39:39 +0700

    add Member model

--- backend/courses_api/models.py
+++ backend/courses_api/models.py
@@ -24,16 +24,29 @@ class Lecturer(models.Model):
 
     class Meta:
         ordering = ['msgv']
-
-class Course(models.Model):
-    msmh = models.CharField(max_lenth=50,unique=True)
-    name = models.CharField(max_lenth=500)
-    description = models.TextField()
-    course_member = models.ManyToManyField(Student,related_name="have_student")
+class Member(models.Model):
+    code = models.CharField(max_length=50,unique=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    image = models.ImageField()
+    is_lecturer = models.BooleanField(default=False)
 
     def __str__(self):
-        return "{0} - {1}".format(self.name,self.msmh)
+        return "{0} - {1}".format(self.name,self.code)
+
+    class Meta:
+        ordering = ['code']
+
+class Course(models.Model):
+    mskh = models.CharField(max_length=50,unique=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+    course_member = models.ManyToManyField(Member,related_name="have_member")
+    created_by = models.ForeignKey(Member, on_delete=models.CASCADE,null=False)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.name,self.mskh)
     
 
     class Meta:
-        ordering = ['msmh']
+        ordering = ['mskh']
