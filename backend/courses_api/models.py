f0a800be0e2be26358acb87c05cf22be07bf5eef
Thi Huỳnh Thái <19522256@gm.uit.edu.vn>
2021-10-14 16:40:20 +0700

    delete student model and lecture model

--- backend/courses_api/models.py
+++ backend/courses_api/models.py
@@ -1,29 +1,6 @@
 from django.db import models
 
-class Student(models.Model):
-    mssv=models.CharField(max_length=50,unique=True)
-    name=models.CharField(max_length=200)
-    email=models.EmailField(max_length=254)
-    image=models.ImageField()
 
-
-    def __str__(self):
-        return "{0} - {1}".format(self.name,self.mssv)
-
-    class Meta:
-        ordering = ['mssv']
-
-class Lecturer(models.Model):
-    msgv=models.CharField(max_length=50,unique=True)
-    name=models.CharField(max_length=200)
-    email=models.EmailField(max_length=254)
-    image=models.ImageField()
-
-    def __str__(self):
-        return "{0} - {1}".format(self.name,self.msgv)
-
-    class Meta:
-        ordering = ['msgv']
 class Member(models.Model):
     code = models.CharField(max_length=50,unique=True)
     name = models.CharField(max_length=200)
