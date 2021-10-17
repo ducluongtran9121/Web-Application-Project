9201b071a8d1e03fe5ff4f4f486ebc2473da4ceb
Thi Huỳnh Thái <19522256@gm.uit.edu.vn>
2021-10-17 22:40:38 +0700

    add coursedetail serializer

--- backend/courses_api/serializers.py
+++ backend/courses_api/serializers.py
@@ -6,10 +6,37 @@ from .models import *
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['id','mskh','name','description']
+        fields = ['id','mskh','name','description','created_by','course_lecturer']
 
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ['id','name']
 class CourseDetailSerializer(serializers.ModelSerializer):
+    course_lesson=LessonSerializer(many=True,read_only=True)
     class Meta:
         model = Course
-        fields = '__all__'
-        depth=1
+        fields = ['id','mskh','name','description','create_at','update_at','created_by','course_lecturer','course_lesson']
+        read_only_fields = ['id','create_at','update_at']
+
+
+    def update(self,instance,validated_data):
+        #lessons_data = validated_data.pop('course_lesson',None)
+        lecturers_data = validated_data.pop('course_lecturer',None)
+        
+        instance = super(CourseDetailSerializer,self).update(instance, validated_data)
+        print(lecturers_data)
+        for lecturer_data in lecturers_data:
+
+            lecturer_qs = Member.objects.filter(id=lecturer_data.id)
+
+            if lecturer_qs.exists():
+                lecturer = lecturer_qs.first()
+            else:
+                lecturer = Member.objects.create(**lecturer_data)
+            
+            instance.course_lecturer.add(lecturer)
+
+        return instance
