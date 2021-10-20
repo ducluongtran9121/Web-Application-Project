c0aae3d3249c2d42054dfaeb646ab7399273c51c
Ngo Duc Hoang Son <57288958+pinanek23@users.noreply.github.com>
2021-10-20 10:00:48 +0700

    Delete backend/alunno_api directory

--- backend/alunno_api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Course)
-admin.site.register(Member)
-admin.site.register(Lesson)
-admin.site.register(File)
--- backend/alunno_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    #spath('',CoursesAPIview.as_view()),
-    #path('<int:pk>',CourseDetailAPIView.as_view()),
-    #path('<int:pk>/lesson',CourseLessonDetailAPIView.as_view())
-]
